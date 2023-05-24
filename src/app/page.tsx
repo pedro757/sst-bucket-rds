a561ceba60ab3f18ce1585f3e4bef709569975e1
Pedro Sanchez <pedro.sanchez.jozic@gmail.com>
2023-05-24 14:20:15 -0400

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import crypto from "crypto";
+import { Bucket } from "sst/node/bucket";
+import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+
+export default async function Page() {
+  const command = new PutObjectCommand({
+    ACL: "public-read",
+    Key: crypto.randomUUID(),
+    Bucket: Bucket.public.bucketName,
+  });
+  const url = await getSignedUrl(new S3Client({}), command);
+
+  return <div>{url}</div>;
+}
---
+++ sst.config.ts
@@ -0,0 +1,24 @@
+import { type SSTConfig } from "sst";
+import { Bucket, NextjsSite } from "sst/constructs";
+
+export default {
+  config(_input) {
+    return {
+      name: "sst-bucket-rds",
+      region: "us-east-1",
+    };
+  },
+  stacks(app) {
+    app.stack(function Site({ stack }) {
+      const bucket = new Bucket(stack, "public");
+
+      const site = new NextjsSite(stack, "site", {
+        bind: [bucket]
+      });
+
+      stack.addOutputs({
+        SiteUrl: site.url,
+      });
+    });
+  },
+} satisfies SSTConfig;
